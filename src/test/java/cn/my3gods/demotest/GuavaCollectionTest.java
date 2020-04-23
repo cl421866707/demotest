635ef91ba0bf01dcec85386226e328eda2ef3604
Charlie <charlie.chen@wingsing.com>
2020-04-23 18:02:23 +0800

    初始化集合

--- src/test/java/cn/my3gods/demotest/GuavaCollectionTest.java
+++ src/test/java/cn/my3gods/demotest/GuavaCollectionTest.java
@@ -1,5 +1,6 @@
 package cn.my3gods.demotest;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Lists;
 import java.util.List;
@@ -16,6 +17,9 @@ public class GuavaCollectionTest {
         // 使用一个经验公式：5L + arraySize + (arraySize / 10)计算创建的List的初始大小
         List<String> strList = Lists.newArrayList("1", "2", "3", "3", "3", "3", "3", "3");
 
+        List<String> list1 = ImmutableList.of("1", "2", "3");
+        List<Object> list2 = ImmutableList.builderWithExpectedSize(4).add("1").add("2").add("3").add("4").build();
+
         System.err.println(strList);
 
     }
@@ -26,7 +30,7 @@ public class GuavaCollectionTest {
     @Test
     public void testMaps() {
         Map<String, String> map1 = ImmutableMap.of("1", "a", "2", "b", "3", "c", "4", "d", "5", "e");
-        Map<Object, Object> map2 = ImmutableMap.builder().put("1", "a").put("2", "b").put("3", "c").put("4", "d").put("5", "e").build();
+        Map<Object, Object> map2 = ImmutableMap.builderWithExpectedSize(8).put("1", "a").put("2", "b").put("3", "c").put("4", "d").put("5", "e").build();
 
         System.err.println(map1);
         System.err.println(map2);
