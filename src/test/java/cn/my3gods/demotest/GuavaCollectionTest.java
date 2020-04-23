b649656d90abb201f9cc51dee98eb842d101a0f7
Charlie <charlie.chen@wingsing.com>
2020-04-23 17:55:17 +0800

    JAVA集合的直接初始化

---
+++ src/test/java/cn/my3gods/demotest/GuavaCollectionTest.java
@@ -0,0 +1,35 @@
+package cn.my3gods.demotest;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+
+public class GuavaCollectionTest {
+
+    /**
+     * 直接初始化有值的List
+     */
+    @Test
+    public void testLists() {
+        // 使用一个经验公式：5L + arraySize + (arraySize / 10)计算创建的List的初始大小
+        List<String> strList = Lists.newArrayList("1", "2", "3", "3", "3", "3", "3", "3");
+
+        System.err.println(strList);
+
+    }
+
+    /**
+     * 直接初始化有值的Map
+     */
+    @Test
+    public void testMaps() {
+        Map<String, String> map1 = ImmutableMap.of("1", "a", "2", "b", "3", "c", "4", "d", "5", "e");
+        Map<Object, Object> map2 = ImmutableMap.builder().put("1", "a").put("2", "b").put("3", "c").put("4", "d").put("5", "e").build();
+
+        System.err.println(map1);
+        System.err.println(map2);
+
+    }
+}
